118830e0ad6624558e3b20dd4e48d56798866d31
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2021-12-22 00:12:14 +0300

    updt

---
+++ ConsumerExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsumerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
